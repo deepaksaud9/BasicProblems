28d0c0618c8b6e19828d84066d1d60fefab74e1a
deepak saud <deppofficial00@gmail.com>
2023-02-18 13:58:21 +0545

    initial commit

---
+++ Core/src/Sum_OfNaturalNumber.java
@@ -0,0 +1,20 @@
+
+//sum of natural numbers
+
+public class Sum_OfNaturalNumber {
+	
+	public static void main(String[] args) {
+		
+	
+	int count =0;
+	
+	for(int i=0;i<10;i++)
+	{
+		count = count +i;
+		
+	}
+	System.out.println(count);
+		
+			
+		}
+	}
