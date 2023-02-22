9b1bdaacf38b0f99ec6b86ed1d9a19e487ed7865
deepak saud <deppofficial00@gmail.com>
2023-02-22 23:09:22 +0545

    add FindDuplicate problem

--- Core/src/CoreJavaBasicProblem/VowelCounting.java
+++ Core/src/CoreJavaBasicProblem/VowelCounting.java
@@ -3,16 +3,32 @@ package coreJavaBasicProblem;
 import java.util.Scanner;
 
 public class VowelCounting {
-	
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
 		
-		String[] st = input.nextLine().split("");
-		
-		for( String depp : st)
-		{
-			System.err.println(depp);
-		}
-	}
+	
+	 Scanner sc = new Scanner(System.in);
+     System.out.println("Enter a list of strings: ");
+     String[] input = sc.nextLine().split("\\s+");
 
-}
+     for (String str : input) {
+         int vowelNumCount = 0;
+         for (int i = 0; i < str.length(); i++) {
+             char ch = Character.toLowerCase(str.charAt(i));
+             if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                 vowelNumCount++;
+             }
+         }
+         if (vowelNumCount == 0) {
+             System.out.println("No Vowels");
+         } else if (vowelNumCount > 10) {
+             System.out.println("Too many Vowels");
+         } else {
+             System.out.println(vowelNumCount);
+         }
+     }
+     }
+ }
+
+
+
+
