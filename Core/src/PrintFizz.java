16e4cc2315c75598eeec0e611589b0f90541b321
deepak saud <deppofficial00@gmail.com>
2023-02-27 03:37:46 +0545

    basic programming

---
+++ Core/src/PrintFizz.java
@@ -0,0 +1,13 @@
+
+public class PrintFizz {
+
+	public static void main(String[] args) {
+		
+//		int fiz[]={}; 
+		for(int i=3;i<100;i=i+3)
+		{
+			int fiz[i];
+			System.out.println(fiz[i]);
+		}
+	}
+}
